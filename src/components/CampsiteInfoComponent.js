83df6e432301842b0fcbb0c955b0ae10b1f41b7d
JuanBurgos <JuanBurgos@outlook.com>
2021-03-19 20:13:52 -0400

    Week4 Workshop Task 1-2 Complete

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, Label, Row, } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors, } from 'react-redux-form';
 
 function RenderCampsite({ campsite }) {
     return (
@@ -20,22 +21,27 @@ function RenderComments({ comments }) {
     if (comments) {
 
         return (
-            <div className="col-md-5 m-1">
-                <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return (
-                        <div>
-                            <p>{comment.text}</p>
+            <React.Fragment>
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map((comment) => {
+                        return (
+                            <div>
+                                <p>{comment.text}</p>
                 -- {comment.author}{" "}
-                            {new Intl.DateTimeFormat("en-US", {
-                                year: "numeric",
-                                month: "short",
-                                day: "2-digit",
-                            }).format(new Date(Date.parse(comment.date)))}
-                        </div>
-                    );
-                })}
-            </div>
+                                {new Intl.DateTimeFormat("en-US", {
+                                    year: "numeric",
+                                    month: "short",
+                                    day: "2-digit",
+                                }).format(new Date(Date.parse(comment.date)))}
+                            </div>
+                        );
+                    })}
+                    <CommentForm />
+                </div>
+
+            </React.Fragment>
+
         );
     } else {
         return <div>No comments available</div>;
@@ -67,5 +73,93 @@ function CampsiteInfo(props) {
     }
 }
 
+class CommentForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="form-group mt-3">
+                    <Button outline color="secondary" onClick={this.toggleModal}><i className="fa fa-pencil fa-lg" /> Submit Comment</Button>
+                </div>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Submit Comment</h5>
+                        </div>
+
+                        <div className="modal-body">
+                            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                                <div className="form-group">
+                                    <Label>Rating</Label>
+                                    <Control.select model=".rating" name="rating" id="rating"
+                                        className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+
+                                        {/* Logic goes here */}
+
+                                    </Control.select>
+                                </div>
+
+                                <div className="form-group" >
+                                    <Label>Your Name</Label>
+                                    <Control.text model=".author" name="author" id="author"
+                                        className="form-control" placeholder="Your Name">
+
+                                        {/* Logic goes here */}
+
+                                    </Control.text>
+                                </div>
+
+                                <div className="form-group">
+                                    <Label>Comment</Label>
+                                    <Control.textarea model=".text" name="text" id="text"
+                                        className="form-control" placeholder="Say something..." rows={6}>
+
+                                        {/* Logic goes here */}
+
+                                    </Control.textarea>
+                                </div>
+                                <Button type="submit" color="primary">
+                                    Submit
+                                    </Button>
+                            </LocalForm>
+
+                        </div>
+
+
+                    </div>
+
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}
 
 export default CampsiteInfo;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -37,7 +37,7 @@ class Main extends Component {
                 <CampsiteInfo
                     campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                     comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-                />
+                    />
             );
         }
 
